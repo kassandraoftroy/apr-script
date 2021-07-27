177e1dcb77c9550c18382c239b8ff111d4b70b30
superarius <superarius@gmail.com>
2021-07-27 15:22:56 +0200

    clean up

--- index.ts
+++ index.ts
@@ -8,10 +8,7 @@ import guniPoolABI from './guniPoolABI'
 import * as dotenv from "dotenv";
 dotenv.config({ path: __dirname + "/.env" });
 
-const ALCHEMY_ID = process.env.ALCHEMY_ID;
-const UNISWAP_HELPERS_ADDRESS = '0xFbd0B8D8016b9f908fC9652895c26C5a4994fE36'
-
-const getAPRFromSnapshots = (snapshots: any) => {
+const computeAPRFromSnapshots = (snapshots: any) => {
   let cumulativeBlocks = 0
   let cumulativeGrowth = 0
   for (let i=1; i<snapshots.length; i++) {
@@ -31,49 +28,42 @@ const getAPRFromSnapshots = (snapshots: any) => {
   return growthPerYear
 }
 
-const getAPRs = async (pools: any) => {
-  const PROVIDER = new ethers.providers.JsonRpcProvider(`https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_ID}`)
-  const liquidityContract = new ethers.Contract(UNISWAP_HELPERS_ADDRESS, ["function getLiquidityForAmounts(uint160,int24,int24,uint256,uint256) external pure returns(uint128)"], PROVIDER)
-  let aprs = []
-  for (let i=0; i<pools.length; i++) {
-    if (pools[i].supplySnapshots.length == 0) {
-      continue
-    }
-    let snapshots = [...pools[i].rebalanceSnapshots].sort((a: any, b:any) => (a.block > b.block) ? 1: -1)
-    let supplySnaps = [...pools[i].supplySnapshots].sort((a: any, b: any) => (a.block < b.block) ? 1: -1)
-    let currentBlock = (await PROVIDER.getBlock('latest')).number
-    if (snapshots.length == 0) {
-      let found = false
-      for (let j=0; j<supplySnaps.length; j++) {
-        if ((Number(supplySnaps[j].block) + 5600 < Number(currentBlock.toString())) && !found) {
-          snapshots.push(supplySnaps[j])
-          found = true
-        }
+const getAPR = async (poolData: any, helpersContract: any, guniPoolContract: any, uniswapPoolContract: any): Promise<number> => {
+  if (poolData.supplySnapshots.length == 0) {
+    return 0
+  }
+  let snapshots = [...poolData.rebalanceSnapshots].sort((a: any, b:any) => (a.block > b.block) ? 1: -1)
+  let supplySnaps = [...poolData.supplySnapshots].sort((a: any, b: any) => (a.block < b.block) ? 1: -1)
+  let currentBlock = (await helpersContract.provider.getBlock('latest')).number
+  if (snapshots.length == 0) {
+    for (let j=0; j<supplySnaps.length; j++) {
+      if (Number(supplySnaps[j].block) + 5600 < Number(currentBlock.toString())) {
+        snapshots.push(supplySnaps[j])
+        break
       }
     }
-    const guniPool = new ethers.Contract(ethers.utils.getAddress(pools[i].id), guniPoolABI, PROVIDER)
-    const uniswapPool = new ethers.Contract(ethers.utils.getAddress(pools[i].uniswapPool), uniswapPoolABI, PROVIDER)
-    const sqrtPriceX96 = (await uniswapPool.slot0()).sqrtPriceX96
-    const gross = await guniPool.getUnderlyingBalances()
-    const liquidity = await liquidityContract.getLiquidityForAmounts(sqrtPriceX96, pools[i].lowerTick, pools[i].upperTick, gross[0], gross[1])
-    const tickSpan = Number(pools[i].upperTick) - Number(pools[i].lowerTick)
-    snapshots.push({
-      block: currentBlock.toString(),
-      totalSupply: pools[i].totalSupply,
-      liquidity: liquidity,
-      tickSpan: tickSpan
-    })
-    const apr = getAPRFromSnapshots(snapshots)
-    console.log(`pool ${pools[i].id.substring(0, 6)}: ${(100*apr).toFixed(3)}%`)
-    aprs.push(apr)
   }
+  if (snapshots.length == 0) {
+    return 0
+  }
+  const sqrtPriceX96 = (await uniswapPoolContract.slot0()).sqrtPriceX96
+  const gross = await guniPoolContract.getUnderlyingBalances()
+  const liquidity = await helpersContract.getLiquidityForAmounts(sqrtPriceX96, poolData.lowerTick, poolData.upperTick, gross[0], gross[1])
+  const tickSpan = Number(poolData.upperTick) - Number(poolData.lowerTick)
+  snapshots.push({
+    block: currentBlock.toString(),
+    totalSupply: poolData.totalSupply,
+    liquidity: liquidity.toString(),
+    tickSpan: tickSpan.toString()
+  })
+  return computeAPRFromSnapshots(snapshots)
+} 
 
-  return aprs
-}
-
-export const fetchAPRs = async (): Promise<number[]> => {
+export const fetchAPRs = async () => {
     const APIURL = "https://api.thegraph.com/subgraphs/name/superarius/guni";
-  
+    const PROVIDER = new ethers.providers.JsonRpcProvider(`https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_ID}`)
+    const UNISWAP_HELPERS_ADDRESS = '0xFbd0B8D8016b9f908fC9652895c26C5a4994fE36'
+
     const obsQ = `
       query {
         pools {
@@ -115,7 +105,17 @@ export const fetchAPRs = async (): Promise<number[]> => {
       query: gql(obsQ)
     })
     const pools = data.data.pools
-    return getAPRs(pools)
+    
+    const helpersContract = new ethers.Contract(UNISWAP_HELPERS_ADDRESS, ["function getLiquidityForAmounts(uint160,int24,int24,uint256,uint256) external pure returns(uint128)"], PROVIDER)
+    console.log("fetching pools...")
+    for (let i=0; i<pools.length; i++) {
+      const guniPoolContract = new ethers.Contract(ethers.utils.getAddress(pools[i].id), guniPoolABI, PROVIDER)
+      const uniswapPoolContract = new ethers.Contract(ethers.utils.getAddress(pools[i].uniswapPool), uniswapPoolABI, PROVIDER)
+
+      const apr = await getAPR(pools[i], helpersContract, guniPoolContract, uniswapPoolContract)
+      
+      console.log(`pool ${pools[i].id.substring(0, 6)}: ${(100*apr).toFixed(3)}%`)
+    }
 }
 
 (async () => {
